aa88b8e9557d1af051ca09aa5fc2e5b9271d7ff1
rohit8020 <rohitgangwar8020@gmail.com>
2021-10-09 20:33:42 +0530

    authentication changes

--- routes/index.js
+++ routes/index.js
@@ -25,18 +25,18 @@ router.post("/register", [
   body('email')
     .isEmail()
     .withMessage('Please enter a valid email address.')
-    .custom((value, { req }) => {
-      if (!value.includes("@")) {
+    .custom((value) => {
+      if(!value.includes("@")) {
         throw new Error('The Email Should valid!');
       }
       return true;
     })
     .normalizeEmail(),
     body('college')
-  .isLength({min:3 , max:200 })
-  .withMessage('The college name should be of minimum 3 and maximum of the length 200 letters!')
-  .trim(),
-  body('password')
+    .isLength({min:3 , max:200 })
+    .withMessage('The college name should be of minimum 3 and maximum of the length 200 letters!')
+    .trim(),
+    body('password')
     .isLength({ min: 5 })
     .isAlphanumeric()
     .withMessage('Password Should Alphanumeric and Minimum of 5 Characters!!')
@@ -48,16 +48,23 @@ router.post("/register", [
       req.flash("error", errors.array()[0].msg);
       return res.redirect('back');
     }
-    var newUser = new User({username: req.body.username, email: req.body.email, college: req.body.college});
+
+    var user={
+      username: req.body.username,
+      email: req.body.email,
+      college: req.body.college
+    }
+    var newUser = new User(user);
+
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             req.flash("error", err.message)
             res.redirect("register");
         }else{
-          passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function(){
             req.flash("success", "Welcome To College-Events " + user.username);
             res.redirect("/login");
-          });
+          })(req,res,next);
         }      
     });
 });
